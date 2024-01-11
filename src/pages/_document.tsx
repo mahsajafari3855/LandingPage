72230f76e9b6193dc46ccce3bfd659a465a694e1
Mahsa Jafari <mahsa@Mahsa-MacBook-Pro.local>
2024-01-11 12:39:24 +0330

    implemented project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* GTM code to be added in the <head> section */}
+
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtm.js?id=GTM-WT7K7LBS`}
+          />
+        </Head>
+        <body>
+          {/* GTM code to be added immediately after the opening <body> tag */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WT7K7LBS"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
